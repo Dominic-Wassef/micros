c445a23d1ceeebd2101545264a7ab26c86fac12b
Aviroute <youturncorporation@gmail.com>
2022-01-16 19:27:12 -0500

    adding

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -2,9 +2,16 @@ import { ValidationError } from 'express-validator';
 import { isConstructorDeclaration } from 'typescript';
 
 export class RequestValidationError extends Error {
+    statusCode = 400;
     constructor(public errors: ValidationError[]) {
         super();
 
         Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+
+    serializeErrors() {
+        return this.errors.map(err => {
+            return { message: err.msg, field: err.param }
+        });
     }
 }
