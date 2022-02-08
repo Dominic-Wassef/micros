6495e825cc81eed88641c67b60edcddd18eee40a
Aviroute <youturncorporation@gmail.com>
2022-02-08 15:33:10 -0500

    adding

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -4,6 +4,8 @@ import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
+
 const router = express.Router();
 
 router.post(
