ee9ad8ad7ae3ec71553ddec3250bca086a068344
Aviroute <youturncorporation@gmail.com>
2022-02-08 15:26:12 -0500

    adding

--- auth/src/middlewares/validate-request.ts
+++ auth/src/middlewares/validate-request.ts
@@ -1,1 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+
+export const validateRequest = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+        
+    }
