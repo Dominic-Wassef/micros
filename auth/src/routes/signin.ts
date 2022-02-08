e9214128d2c71d7a50a81b8445c3123e5923bd6d
Aviroute <youturncorporation@gmail.com>
2022-02-08 16:25:26 -0500

    existingUser

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { Jwt } from 'jsonwebtoken';
 
+import { Password } from '../services/password';
 import { User } from '../models/user'; 
 import { validateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -25,6 +27,25 @@ router.post('/api/users/signin',
         if (!existingUser) {
             throw new BadRequestError('Invalid credentials');
         }
+
+        const passwordsMatch = await Password.compare(
+            existingUser.password, 
+            password
+        );
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid credentials');
+        }
+        const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+        }, 
+        process.env.JWT_KEY! 
+        );
+        req.session = {
+        jwt: userJwt
+        };
+
+        res.status(200).send(existingUser);
     }
 );
 
