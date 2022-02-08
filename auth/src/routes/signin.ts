2995e6b6a9432cab9cbf8e40a2f3b0ee2930c683
Aviroute <youturncorporation@gmail.com>
2022-02-08 16:28:00 -0500

    syntax fix

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -21,32 +21,32 @@ router.post('/api/users/signin',
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-        const { email, password } = req.body;
+    const { email, password } = req.body;
 
-        const existingUser = await User.findOne({ email });
-        if (!existingUser) {
-            throw new BadRequestError('Invalid credentials');
-        }
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+        throw new BadRequestError('Invalid credentials');
+}
 
-        const passwordsMatch = await Password.compare(
-            existingUser.password, 
-            password
-        );
-        if (!passwordsMatch) {
-            throw new BadRequestError('Invalid credentials');
-        }
-        const userJwt = jwt.sign({
-        id: existingUser.id,
-        email: existingUser.email
-        }, 
-        process.env.JWT_KEY! 
-        );
-        req.session = {
-        jwt: userJwt
-        };
+    const passwordsMatch = await Password.compare(
+        existingUser.password, 
+        password
+);
+    if (!passwordsMatch) {
+        throw new BadRequestError('Invalid credentials');
+}
+    const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email
+}, 
+    process.env.JWT_KEY! 
+);
+    req.session = {
+    jwt: userJwt
+};
 
-        res.status(200).send(existingUser);
-    }
+    res.status(200).send(existingUser);
+}
 );
 
 export { router as signinRouter }; 
