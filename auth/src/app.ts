665f0c7cc9a9637185645a1566d621c5af0f7489
Aviroute <youturncorporation@gmail.com>
2022-02-16 19:45:11 -0500

    adding

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,7 +1,6 @@
-import express from "express";
+import express from 'express';
 import 'express-async-errors';
-import { json } from "body-parser";
-
+import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
@@ -9,10 +8,10 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from "./errors/not-found-error";
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
-app.set('trust proxy', true)
+app.set('trust proxy', true);
 app.use(json());
 app.use(
   cookieSession({
@@ -26,7 +25,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.get('*', async (req, res) => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
