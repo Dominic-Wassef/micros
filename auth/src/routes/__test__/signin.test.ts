aeb16e9e51f3ddb3d15038ced99f73c1a65beeb3
Aviroute <youturncorporation@gmail.com>
2022-02-17 02:29:38 -0500

    adding

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,22 +1,55 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+jest.useRealTimers();
+
 it('fails when a email that does not exist is supplied', async () => {
-    jest.setTimeout(50000);
+    jest.setTimeout(60000);
     await request(app)
     .post('/api/users/signin')
     .send({
-        email: 'test@test.com',
-        password: 'password'
+      email: 'test@test.com',
+      password: 'password'
     })
     .expect(400);
-})
+});
 
 it('fails when an incorrect password is supplied', async () => {
+    jest.setTimeout(60000);
     await request(app)
     .post('/api/users/signup')
     .send({
-        email: 'tests@test.com',
-        password: 'password'
+      email: 'test@test.com',
+      password: 'password'
     })
-})
+    .expect(201);
+
+    await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'aslkdfjalskdfj'
+    })
+    .expect(400);
+});
+
+it('responds with a cookie when given valid credentials', async () => {
+    jest.setTimeout(60000);
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+    const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+}, 60000);
