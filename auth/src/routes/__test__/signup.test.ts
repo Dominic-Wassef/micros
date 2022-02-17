837d4a72c1660788d2015f04a75e98a4f8f38ed0
Aviroute <youturncorporation@gmail.com>
2022-02-17 02:29:47 -0500

    adding

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,84 +1,86 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+jest.useRealTimers();
+
 it('returns a 201 on successful signup', async () => {
-    jest.setTimeout(50000);
+    jest.setTimeout(60000);
     return request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201);
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
 });
 
 it('returns a 400 with an invalid email', async () => {
-    jest.setTimeout(50000);
+    jest.setTimeout(60000);
     return request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'alskdflaskjfd',
-            password: 'password'
-        })
-        .expect(400);
+    .post('/api/users/signup')
+    .send({
+      email: 'alskdflaskjfd',
+      password: 'password'
+    })
+    .expect(400);
 });
 
 it('returns a 400 with an invalid password', async () => {
-    jest.setTimeout(50000);
+    jest.setTimeout(60000);
     return request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'alskdflaskjfd',
-            password: 'p'
-        })
-        .expect(400);
+    .post('/api/users/signup')
+    .send({
+      email: 'alskdflaskjfd',
+      password: 'p'
+    })
+    .expect(400);
 });
 
-it('returns a 400 with an missing email and password', async () => {
-    jest.setTimeout(50000);
+it('returns a 400 with missing email and password', async () => {
+    jest.setTimeout(60000);
     await request(app)
     .post('/api/users/signup')
     .send({
-        email: 'test@test.com',
+      email: 'test@test.com'
     })
     .expect(400);
-    
-    return request(app)
-        .post('/api/users/signup')
-        .send({
-            password: 'alskjdk',
-        })
-        .expect(400);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'alskjdf'
+    })
+    .expect(400);
 });
 
 it('disallows duplicate emails', async () => {
-    jest.setTimeout(50000);
+    jest.setTimeout(60000);
     await request(app)
     .post('/api/users/signup')
     .send({
-        email: 'test@test.com',
-        password: 'password'
+      email: 'test@test.com',
+      password: 'password'
     })
     .expect(201);
 
     await request(app)
     .post('/api/users/signup')
     .send({
-        email: 'test@test.com',
-        password: 'password'
+      email: 'test@test.com',
+      password: 'password'
     })
     .expect(400);
 });
 
 it('sets a cookie after successful signup', async () => {
-    jest.setTimeout(50000);
+    jest.setTimeout(60000);
     const response = await request(app)
     .post('/api/users/signup')
     .send({
-        email: 'test@test.com',
-        password: 'password'
+      email: 'test@test.com',
+      password: 'password'
     })
     .expect(201);
 
     expect(response.get('Set-Cookie')).toBeDefined();
-});
+}, 60000);
