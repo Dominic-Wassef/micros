f8cfb87d63f55efd3294d7d7061ce69f670cda6a
Aviroute <youturncorporation@gmail.com>
2022-02-17 19:52:29 -0500

    adding

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,29 +1,54 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+      function signin(): Promise<string[]>;
+    }
 
 let mongo: any;
 beforeAll(async () => {
+    jest.useFakeTimers('legacy')
     process.env.JWT_KEY = 'asdfasdf';
-    
-    mongo = await MongoMemoryServer.create();
-    const mongoUri = mongo.getUri();
 
-    await mongoose.connect(mongoUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 });
 
 beforeEach(async () => {
+    jest.useFakeTimers('legacy')
     const collections = await mongoose.connection.db.collections();
 
     for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
+    jest.useFakeTimers('legacy')
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
