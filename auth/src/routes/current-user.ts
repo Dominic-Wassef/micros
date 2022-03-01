a786ae6ae0bbc75ec5167e2dcd81f41e76b6971e
Aviroute <youturncorporation@gmail.com>
2022-02-28 23:51:24 -0500

    adding

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@org103849343954/common';
 
 const router = express.Router();
 
