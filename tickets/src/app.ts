ca8d9595f29da099c9688ac48a16b969690ec6a6
Aviroute <youturncorporation@gmail.com>
2022-03-08 22:49:26 -0500

    adding

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,7 +5,7 @@ import cookieSession from 'cookie-session';
 import { currentUser, errorHandler, NotFoundError } from '@org103849343954/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes';
+import { indexTicketRouter } from './routes/index';
 import { updateTicketRouter } from './routes/update';
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: process.env.NODE_ENV !== 'test',
   })
 );
 app.use(currentUser);
