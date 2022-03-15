f5f5d7ddc95b5602eaa6e21535770c7d103fba20
Aviroute <youturncorporation@gmail.com>
2022-03-14 22:40:11 -0400

    adding

---
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketCreatedEvent } from '@org103849343954/common';
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+    readonly subject = Subjects.TicketCreated;
+  }
