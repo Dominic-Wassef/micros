ceaf7cfcf92451e285f225b97cf3be8b32f9b460
Aviroute <youturncorporation@gmail.com>
2022-03-19 02:42:30 -0400

    adding

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -12,7 +12,7 @@ it('fetches the order', async () => {
 
     const user = global.signin();
     // Make a request to build an order with this ticket
-    await request(app)
+    const { body: order } = await request(app)
         .post('/api/orders')
         .set('Cookie', user)
         .send({ ticketId: ticket.id })
@@ -20,4 +20,36 @@ it('fetches the order', async () => {
 
 
     // Make request to fetch the order
-})
+    const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+    expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('returns an error if one user tries to fetch another users order', async () => {
+    // Create a ticket
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const user = global.signin();
+    // Make a request to build an order with this ticket
+    const { body: order } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+
+    // Make request to fetch the order
+    await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401);
+});
