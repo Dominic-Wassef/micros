4ec4cef42e6adae09dfd76856ecfb50b86651df9
Aviroute <youturncorporation@gmail.com>
2022-03-30 23:37:25 -0400

    adding

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,39 +1,46 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import {
-    validateRequest,
-    BadRequestError,
-    NotAuthorizedError,
-    NotFoundError,
-    requireAuth,
-    OrderStatus
+  requireAuth,
+  validateRequest,
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
 } from '@org103849343954/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.post(
-    '/api/payments', 
-    requireAuth,
-    [body('token').not().isEmpty(),body('orderId').not().isEmpty(),],
-    validateRequest,
-    async (req: Request, res: Response) => {
-        const { token, orderId } = req.body;
+  '/api/payments',
+  requireAuth,
+  [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
 
-        const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId);
 
-        if (!order) {
-            throw new NotFoundError();
-        }
-        if (order.userId !== req.currentUser!.id) {
-            throw new NotAuthorizedError();
-        }
-        if (order.status === OrderStatus.Cancelled) {
-            throw new BadRequestError('Cannot pay for a cancelled order');
-        }
-
-        res.send({ success: true });
+    if (!order) {
+      throw new NotFoundError();
     }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: 'tok_visa',
+    });
+
+    res.send({ success: true });
+  }
 );
 
 export { router as createChargeRouter };
