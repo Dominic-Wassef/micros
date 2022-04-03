5d247452f6759594af2bfe89090f27f94d0433ea
Aviroute <youturncorporation@gmail.com>
2022-04-03 14:22:21 -0400

    adding

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,34 +1,56 @@
 import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
-const OrderShow = ({ order }) => {
-    const [timeLeft, setTimeLeft] = useState(0);
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: () => Router.push('/orders'),
+  })
 
-    useEffect(() => {
-        const findTimeLeft = () => {
-            const msLeft = new Date(order.expiresAt) - new Date();
-            setTimeLeft(Math.round(msLeft / 1000));
-        };
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
 
-        findTimeLeft();
-        const timerId = setInterval(findTimeLeft, 1000);
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
 
-        return () => {
-            clearInterval(timerId);
-        };
-    }, [order]);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
 
-    if (timeLeft < 0) {
-        return <div>Order Expired</div>
-    }
-    
-    return <div>Time left to pay: {timeLeft} seconds</div>;
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey="pk_test_51IUlHwKR8JTyttbVx1Q76v6hU6GD0CiXGsDVyaMcfV57G3xPcSWq8BsqqkycfXMl4CE6pe0t1agktg6fzeUbvXBV00wAkEQt1K"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
-    const {orderId} = context.query;
-    const { data } = await client.get(`/api/orders/${orderId}`);
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
 
-    return { order: data };
+  return { order: data };
 };
 
 export default OrderShow;
